605f4018ee07157b9a3bd10cdbac3d4116f40b43
yogendra saxena <yogendrasaxena56@gmail.com>
2021-07-29 14:32:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ app.use(cors());
 
 const port = process.env.PORT || 7777;
 
-app.use("", async (req, res) => {
+app.use(async (req, res) => {
   try {
-    const response = await fetch(
-      `https://something/${req.url}`,
-      {
-        method: req.method,
-        headers: req.headers,
-      }
-    );
+    console.log("Running proxy Server");
+    const url = req.url.slice(1);
+    const response = await fetch(url, {
+      method: req.method,
+      headers: req.headers,
+    });
     res.json(await response.json());
   } catch (error) {
     console.log({ error });
