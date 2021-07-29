766edb8e7c97c43ee07bf65b07856b5f195fc6ea
yogendra saxena <yogendrasaxena56@gmail.com>
2021-07-29 20:49:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const fetch = require("node-fetch");
+
+// var allowedOrigins = ["http://somethingsomething.com", "https://somethinggsomething.com"];
+// // https://medium.com/zero-equals-false/using-cors-in-express-cac7e29b005b
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true); // allowing
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false); // blocking
+//       }
+//       return callback(null, true); //allowing
+//     },
+//   })
+// );
+
 app.use(cors());
 
 const port = process.env.PORT || 7777;
