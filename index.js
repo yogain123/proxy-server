794c0eed24250bb93e21d5a123fe733cb214d34e
yogendra <yogendra.saxena@walmart.com>
2021-07-30 11:40:08 +0530

    git cors others fix

--- index.js
+++ index.js
@@ -2,28 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const fetch = require("node-fetch");
-
-// var allowedOrigins = ["http://somethingsomething.com", "https://somethinggsomething.com"];
-// // https://medium.com/zero-equals-false/using-cors-in-express-cac7e29b005b
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // allow requests with no origin
-//       // (like mobile apps or curl requests)
-//       if (!origin) return callback(null, true); // allowing
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         var msg =
-//           "The CORS policy for this site does not " +
-//           "allow access from the specified Origin.";
-//         return callback(new Error(msg), false); // blocking
-//       }
-//       return callback(null, true); //allowing
-//     },
-//   })
-// );
-
+var bodyParser = require("body-parser");
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
-
 const port = process.env.PORT || 7777;
 
 app.use(async (req, res) => {
@@ -33,6 +14,7 @@ app.use(async (req, res) => {
     const response = await fetch(url, {
       method: req.method,
       headers: req.headers,
+      body: JSON.stringify(req.body),
     });
     res.json(await response.json());
   } catch (error) {
